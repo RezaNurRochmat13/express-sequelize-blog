72537d1de589127962ecac0ea04c91cfd7dafcd8
Reja <rezanurrochmat@gmail.com>
2022-04-20 23:09:17 +0700

    Add migration car

---
+++ migrations/20220420160803-create-car.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama: {
+        type: Sequelize.STRING
+      },
+      sewa: {
+        type: Sequelize.INTEGER
+      },
+      ukuran: {
+        type: Sequelize.STRING
+      },
+      foto: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cars');
+  }
+};
