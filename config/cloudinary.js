de5ee246e1fd6201b2e9f3d9b43c1e761ddb0d46
Reja <rezanurrochmat@gmail.com>
2024-06-13 11:02:44 +0700

    update cloudinary config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,13 @@
 // @ts-ignore
 const cloudinary = require('cloudinary').v2;
+// @ts-ignore
+const dotenv = require('dotenv');
+dotenv.config();
 
 cloudinary.config({
-	cloud_name: 'eterno',
-	api_key: '157965674852962',
-	api_secret: 'WPMAbQ06AvReY1oumvyg0BKK1nk',
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
